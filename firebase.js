a0f80bbbdcde411c2f63c2b5758ae2f790633e02
Suryansh <100128537+suryansh639@users.noreply.github.com>
2024-03-31 22:48:41 +0530

    main

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAbLPg52L-GuIzQqhVPO3JZXfKTi85lytM",
+	authDomain: "myportfolio-f9d82.firebaseapp.com",
+	projectId: "myportfolio-f9d82",
+	storageBucket: "myportfolio-f9d82.appspot.com",
+	messagingSenderId: "359255569473",
+	appId: "1:359255569473:web:59cb3c1b340fc3587382c0",
+	measurementId: "G-MTMDYMZ7HD"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
